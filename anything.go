f3f89f60e906da84627d64aaeb64e51c90858928
chatenilesh <chatenilesh@yahoo.in>
2018-12-01 09:41:40 +0530

    commit 2

--- anything.go
+++ anything.go
@@ -6,6 +6,16 @@ func main() {
 	ret, err := fmt.Println("Hello World")
 	fmt.Println(ret, err)
 
-	ret1, _ := fmt.Println("Hello World")
+	ret1, err := fmt.Println("short declaration")
+	fmt.Println(ret1, err)
+
+	ret1, err = fmt.Println("no declaration")
+	fmt.Println(ret1, err)
+
+	ret1, _= fmt.Println("return throw away")
 	fmt.Println(ret1)
 }
+
+
+//ebnf (Extended Backus-Naur form)
+//identifier = letter {letter | unicode_digit}
