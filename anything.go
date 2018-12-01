a09801f25f8773df59710e64a9b2d3b682bab881
chatenilesh <chatenilesh@yahoo.in>
2018-12-01 09:10:37 +0530

    first program done

--- anything.go
+++ anything.go
@@ -1,1 +1,26 @@
-package forexample
+package main;
+
+import "fmt";
+
+func main() {
+	fmt.Println("Hi");
+	foo();
+	fmt.Println("After foo");
+
+	for i:=0;i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Print(i, " ");
+		}
+	}
+
+	bar();
+}
+
+func foo() {
+	fmt.Println("I am in foo");
+}
+
+
+func bar() {
+	fmt.Println("\nAnd then we exited")
+}
